122e33f4a4a1256f904ba13fc1cb3469de172caa
zons2763 <kdeld1026@gmail.com>
2025-09-02 17:00:37 +0900

    Commit

---
+++ docs/src/nav.js
@@ -0,0 +1,8 @@
+fetch("./nav.html")
+    .then((res) => res.text())
+    .then((text) => {
+        let oldElement = document.querySelector("script#replace_with_navbar");
+        let newElement = document.createElement("div");
+        newElement.innerHTML = text;
+        oldElement.parentNode.replaceChild(newElement, oldElement);
+    });
